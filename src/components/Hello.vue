dabc12bb46f2344bb5217e8e78761fe8cdcecae4
jiabin_xu <631392782@qq.com>
2017-03-01 08:00:25 +0000

    first commit

---
+++ src/components/Hello.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <span class="title">
+      欢迎登录
+    </span>
+    <Divider>Vue Vux</Divider>
+    <Grpup>
+      <XInput title="账号" v-model="account" placeholder="账号" type="text"></XInput>
+    </Grpup>
+    <x-input title="密码" v-model="password" placeholder="密码" type="password"></x-input>
+    <XButton type="primary" action-type="button">登录</XButton>
+  </div>
+</template>
+
+<script>
+import { Divider, Group, XInput, XButton } from 'vux'
+export default {
+  components: {
+    Divider,
+    Group,
+    XInput,
+    XButton
+  },
+  data () {
+    return {
+      addount: '',
+      password: ''
+    }
+  }
+}
+</script>
+
